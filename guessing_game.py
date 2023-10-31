0d7cf16fa22a09e6a3d6b1acfc1fc93019d03158
Michael <59299019+csnewb@users.noreply.github.com>
2023-10-31 09:41:43 -0700

    removed regressive game loop

--- guessing_game.py
+++ guessing_game.py
@@ -7,30 +7,39 @@ import random
 import statistics
 
 
-# Create the start_game function.
-# Write your code inside this function.
-def start_game(attempts_list=[]):
+def main_menu(attempts_list):
+    while True:
+        choice = get_menu_choice()
+        if choice == 1:
+            attempts_list = start_game(attempts_list)
+        elif choice == 2:
+            print("Thanks for playing! Goodbye")
+            break
+
+
+def start_game(attempts_list):
     keep_playing = True
     guesses = 0
 
-    if attempts_list:
-        print(f"Welcome! Curent High Score: {min(attempts_list)}")
+    if len(attempts_list) > 0:
+        print(f"\nLet's begin! Current High Score: {min(attempts_list)}")
     else:
-        print(f"Welcome!")
+        print(f"\nLet's begin!")
     max_number = get_max_number()
     solution = random.randint(1, max_number)
     guess_limit = round((max_number / 20) + 3)
-    print(f"You will receive {guess_limit} guesses")
+    print(f"You will receive {guess_limit} guesses\n")
 
     while keep_playing:
         try:
             choice = get_guess(max_number)
             guesses += 1
             if choice == solution:
-                game_over('win', attempts_list, guesses)
+                updated_attempts_list = game_over('win', attempts_list, guesses)
+                return updated_attempts_list
             if guesses == guess_limit:
-                keep_playing = game_over('lose', attempts_list, guesses)
-                break
+                updated_attempts_list = game_over('lose', attempts_list, guesses)
+                return updated_attempts_list
             player_feedback(guesses, guess_limit, choice, solution)
 
         except ValueError as e:
@@ -42,15 +51,11 @@ def start_game(attempts_list=[]):
 def game_over(outcome, attempts_list, guesses):
     if outcome == 'win':
         print("Congratulations! You Won")
+    else:
+        print("Sorry - Too many incorrect guesses. Game Over.")
     attempts_list.append(guesses)
     display_data(attempts_list, guesses)
-
-    play_again = input("Press 'Y' to play again:  ")
-    if play_again.lower() == 'y':
-        start_game(attempts_list=attempts_list)
-    else:
-        print("Goodbye")
-        return False
+    return attempts_list
 
 
 def display_data(attempts_list, guesses):
@@ -79,10 +84,24 @@ def get_max_number():
             print("Please enter an integer greater than 0")
 
 
+def get_menu_choice():
+    while True:
+        try:
+            print("\nWelcome to the Number Guessing Game")
+            print("1 to Play")
+            print("2 to Exit")
+            choice = int(input("Choose an option (1/2): "))
+            if choice == 1 or choice == 2:
+                return choice
+            else:
+                raise ValueError
+        except ValueError:
+            print("Please enter an integer of 1 or 2")
+
 def get_guess(max_number):
     while True:
         try:
-            prompt = "Please guess a number:  "
+            prompt = f"Please guess a number between 1 and {max_number}:  "
             choice = int(input(prompt))
             if choice <= 0:
                 raise ValueError("Out of Range: Number must be greater than 0")
@@ -98,12 +117,12 @@ def get_guess(max_number):
 def player_feedback(guesses, guess_limit, choice, solution):
     guesses_status = f"Guesses Used: {guesses}  | Guesses Remaining: {guess_limit - guesses}"
     if choice > solution:
-        print(f"It's lower. Your choice of {choice} is too high \n{guesses_status} ")
+        print(f"\nIt's lower. Your choice of {choice} is too high \n{guesses_status} ")
     else:
-        print(f"It's higher. Your choice of {choice} is too low \n{guesses_status}")
+        print(f"\nIt's higher. Your choice of {choice} is too low \n{guesses_status}")
 
 
 
 if __name__ == "__main__":
-    start_game()
+    main_menu([])
 
